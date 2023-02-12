f0373373e5e50db726754692df0546a9ead8d50f
smilingD13 <agarciagomez13@gmail.com>
2023-02-11 22:16:53 -0600

    cambios 2

--- test_decorator.php
+++ test_decorator.php
@@ -15,7 +15,31 @@ $newExtraWhooper = $newWhooper->setDecorator(new Decorator("Extra queso",30));
 
 //echo $newExtraWhooper->getConcepto().' ----------------- +';
 
-echo ($newExtraWhooper->getConcepto());
-echo '<br>-------------------------------------------------------<br>';
-echo "TOTAL --------------------".($newExtraWhooper->getPrecio());
 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patron de Decoradores</title>
+</head>
+<body>
+    <h1>Factura</h1>
+    <div>
+        <?php echo ($newExtraWhooper->getConcepto()); ?>
+    </div>
+    <div>
+    <br>-------------------------------------------------------<br>
+    </div>
+    <div>
+        TOTAL --------------------<?php echo ($newExtraWhooper->getPrecio());?>
+    </div>
+    
+
+    
+</body>
+</html>
